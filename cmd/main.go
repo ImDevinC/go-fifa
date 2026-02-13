c2e6c1996ba3de0fc227ff35f02faa0e91cf85bd
Devin Collins <3997333+ImDevinC@users.noreply.github.com>
2026-02-12 20:49:35 -0800

    Add cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,82 +1,29 @@
+// action: create
 package main
 
 import (
 	"fmt"
 	"log"
-	"strings"
 
-	fifa "github.com/imdevinc/go-fifa"
+	"github.com/ImDevinC/go-fifa/internal/team"
 )
-
-const (
-	competitionId string = "17" // 17 world cup
-)
-
-func showCompetitions(client *fifa.Client) error {
-	comps, err := client.GetCompetitions()
-	if err != nil {
-		return err
-	}
-	for _, c := range comps {
-		log.Printf("Id: %s", c.Id)
-		log.Printf("Name: %s", c.Name[0].Description)
-	}
-	return nil
-}
-
-func getCompetitionsByName(client *fifa.Client, input string) error {
-	comps, err := client.GetCompetitions()
-	if err != nil {
-		return err
-	}
-	input = strings.ToLower(input)
-	var results []fifa.Competition
-	for _, c := range comps {
-		name := strings.ToLower(c.Name[0].Description)
-		if strings.Contains(name, input) {
-			results = append(results, c)
-		}
-	}
-	if len(results) > 1 {
-		fmt.Println("Multiple tournaments returned")
-		fmt.Printf("%+v\n", results)
-	} else if len(results) == 0 {
-		fmt.Println("Found " + results[0].Name[0].Description)
-	} else {
-		fmt.Println("No results found")
-	}
-	return nil
-}
-
-func getLiveMatches(client *fifa.Client) error {
-	matches, err := client.GetCurrentMatches()
-	if err != nil {
-		return err
-	}
-	for _, m := range matches {
-		fmt.Printf("%s: %s vs %s (%s/%s/%s/%s)\n", m.CompetitionName[0].Description, m.HomeTeam.Name[0].Description, m.AwayTeam.Name[0].Description, m.CompetitionId, m.SeasonId, m.StageId, m.MatchId)
-	}
-	return nil
-}
-
-func getMatchData(client *fifa.Client) error {
-	events, err := client.GetMatchEvents(&fifa.GetMatchEventOptions{
-		CompetitionId: "2000000005",
-		SeasonId:      "400250052",
-		StageId:       "b1ayaoa4q68n6464fy4orklqs",
-		MatchId:       "3y748w6ppuxciynnoonrt9jx0",
-	})
-	if err != nil {
-		return err
-	}
-	fmt.Printf("%+v\n", events)
-	return nil
-}
 
 func main() {
-	client := &fifa.Client{}
-	_, err := client.SearchCompetition(&fifa.SearchOptions{Name: "FIFA World Cup"})
+	tm := team.NewTeamManager()
+
+	// Add teams
+	t1 := tm.AddTeam("Team Alpha")
+	t2 := tm.AddTeam("Team Beta")
+	fmt.Println("Added teams:", t1, t2)
+
+	// List teams
+	teams := tm.ListTeams()
+	fmt.Println("List of teams:", teams)
+
+	// Remove a team
+	err := tm.RemoveTeam(t1.ID)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error removing team: %v", err)
 	}
+	fmt.Println("After removal, list of teams:", tm.ListTeams())
 }
